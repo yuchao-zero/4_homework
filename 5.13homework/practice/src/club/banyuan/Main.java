6672c9d9aa96c980f689924dba9f277fa1689f8c
yuchao <18315391050@163.com>
2020-05-14 01:18:47 +0800

    EnumPractic

--- 5.13homework/practice/src/club/banyuan/Main.java
+++ 5.13homework/practice/src/club/banyuan/Main.java
@@ -11,13 +11,22 @@ public class Main {
     public static void main(String[] args) {
         //静态values（）方法，该方法返回枚举中值的数组
         for (Weekday o:Weekday.values()
-             ) {
+        ) {
             printMethod(o);
         }
         Weekday sat = Weekday.SATURDAY;
         for (Weekday day:Weekday.values()
         ) {
             System.out.println(day.compareTo(sat));
+            if(day.compareTo(sat) > 0){
+                System.out.println("大于sat");
+            }
+            if(day.compareTo(sat) < 0) {
+                System.out.println("小于sat");
+            }
+            if(day.compareTo(sat) == 0){
+                System.out.println("等于sat");
+            }
         }
     }
 }
